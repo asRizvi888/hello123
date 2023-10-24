8a4c1b99a357e95bbe2ccedba6e8cc45d1e1734f
asrizvi888 <asrizvi2357@gmail.com>
2023-10-25 00:50:47 +0600

    socket tesing ✅

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {SafeAreaView, Pressable, Text, StyleSheet, Alert} from 'react-native';
 
 import io from 'socket.io-client';
+const customId = 'myCustomId';
 
 const BASE_URL = 'https://www.qvesty.online';
 // const BASE_URL = 'http://localhost:3000';
@@ -11,15 +12,35 @@ const socketIO = io(BASE_URL);
 const App: React.FC<React.ReactNode> = ({props}: any) => {
   const onPressBtn = () => {
     console.log('pressed');
-    socketIO.emit('message', 'gogo');
+    socketIO.emit('message', 'Hello, @qvesty👋🏽');
   };
 
   React.useEffect(() => {
+    // socketIO.on('message', msg => {
+    //   if (msg) {
+    //     Alert.alert('SERVER RESPONSE', msg);
+    //   }
+    //   return () => socketIO.disconnect();
+    // });
+    // Set the custom ID when the connection is established
+    socketIO.on('connect', () => {
+      socketIO.emit('REGISTER_CLIENT', customId);
+    });
+
+    // Handle other events as needed
+    socketIO.on('disconnect', () => {
+      console.log('Disconnected from the server');
+    });
+
+    socketIO.on('custom_event', data => {
+      console.log(`Received custom event: ${data}`);
+    });
+
     socketIO.on('message', msg => {
       if (msg) {
         Alert.alert('SERVER RESPONSE', msg);
       }
-      return () => socketIO.disconnect();
+      // return () => socketIO.disconnect();
     });
   }, []);
 
